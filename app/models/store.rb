ae4e5f304cfb90c549e419fcddecff1799f183b2
junara <jun5araki@gmail.com>
2019-03-10 20:05:44 +0900

    Added import

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,19 @@
+require 'csv'
 class Store < ApplicationRecord
+  def self.import_csv(file)
+    list = []
+    ActiveRecord::Base.transaction do
+      CSV.foreach(file, encoding: 'cp932', headers: true) do |row|
+        list << self.new(store_params(row))
+      end
+      import list
+    end
+  end
+
+  def self.store_params(row)
+    {
+        name: row['名 前'],
+        description: row['販売品目']
+    }
+  end
 end
