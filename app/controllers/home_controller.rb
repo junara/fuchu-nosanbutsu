af6f06327c4a1167fdd46f51cf0468c67db998de
junara <jun5araki@gmail.com>
2019-03-22 08:25:20 +0900

    Added selectize

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @stores = Store.ransack(description_cont: params[:keyword]).result
-    # raise
+    @stores = Store.ransack(description_cont_all: split_queries(params[:keyword])).result
+  end
+
+  private
+
+  def split_queries(str)
+    str.split(',')
   end
 end
